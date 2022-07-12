45329bb41d25f7e719960456785cbb493ad8bb61
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-07-11 21:21:37 -0600

    udpate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 function App() {
+    const title = 'Blog Post'
+    const body  = 'This is my blog post'
+    const comments = [
+        {id: 1, text:'Comment one'},
+        {id: 2, text:'Comment two'},
+        {id: 3, text:'Comment three'},
+        {id: 4, text:'Comment four'},
+
+        
+    ]
+
     return (
-        <h1>Hello From the App component</h1>
+        <div className="container">
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+            <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((comment,index) => (
+                        <li key={index}>{comment.text}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     )
 }
 
