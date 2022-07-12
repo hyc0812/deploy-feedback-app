74cef4d905f077bffb13439dc1c43c5d91459a11
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-07-11 23:55:01 -0600

    update to 16

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+
 function App() {
-    const title = 'Blog Post'
-    const body  = 'This is my blog post'
-    const comments = [
-        {id: 1, text:'Comment one'},
-        {id: 2, text:'Comment two'},
-        {id: 3, text:'Comment three'},
-        {id: 4, text:'Comment four'},
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+  return (
+    <>
+      <Header />
 
-        
-    ]
-
-    return (
-        <div className="container">
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((comment,index) => (
-                        <li key={index}>{comment.text}</li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    )
+      <div className="container">
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
