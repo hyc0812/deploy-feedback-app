7e22ecea28a3c946bf066f8a70ac509ba7071f40
yongchang he <hyc0812@163.com>
2022-07-12 14:03:47 -0600

    add shared button component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import Card from './shared/Card'
 import { useState } from 'react'
+import Button from './shared/Button'
 
 export default function FeedbackForm(props) {
     
@@ -14,8 +15,7 @@ export default function FeedbackForm(props) {
                 <h2>How would you rate your service with us?</h2>
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" placeholder='write a review' value={text}/>
-                    
-                    <button type="submit">Send</button>
+                    <Button type="submit" version='secondary'>Send</Button>
                 </div>
             </form>
         </Card>
