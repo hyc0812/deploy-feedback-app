0244ae4f85c3dbae292dc163ec4dc659dcc1eb93
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-07-12 21:42:13 -0600

    udpate aboutpage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -20,15 +22,30 @@ function App() {
     }
   };
   return (
-    <>
+    <Router>
       <Header />
 
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          />
+
+          <Route exact path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
